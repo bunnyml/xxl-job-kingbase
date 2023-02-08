65b8728c624a85311c025cc70809afde335b0430
杨哲 <yangz@suirui.com>
2023-02-08 11:15:22 +0800

    xxl-job 2.3.1适配金仓kingbaseV8R3八版本

---
+++ xxl-job-admin/src/test/java/com/xxl/job/admin/dao/XxlJobRegistryDaoTest.java
@@ -0,0 +1,31 @@
+package com.xxl.job.admin.dao;
+
+import com.xxl.job.admin.core.model.XxlJobRegistry;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class XxlJobRegistryDaoTest {
+
+    @Resource
+    private XxlJobRegistryDao xxlJobRegistryDao;
+
+    @Test
+    public void test(){
+        int ret = xxlJobRegistryDao.registryUpdate("g1", "k1", "v1", new Date());
+        if (ret < 1) {
+            ret = xxlJobRegistryDao.registrySave("g1", "k1", "v1", new Date());
+        }
+
+        List<XxlJobRegistry> list = xxlJobRegistryDao.findAll(1, LocalDateTime.now().toString());
+
+        int ret2 = xxlJobRegistryDao.removeDead(Arrays.asList(1));
+    }
+
+}
